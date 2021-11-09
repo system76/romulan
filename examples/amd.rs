f5fea8e8c175d1b5ed7b8c71f604895fe35d13a7
Jeremy Soller <jeremy@system76.com>
2021-11-09 10:34:52 -0700

    Add export of PSP and BIOS information

--- examples/amd.rs
+++ examples/amd.rs
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-use std::{env, fs, process};
+use std::{env, fs, path::PathBuf, process};
 
 use romulan::amd::{
     Rom,
     directory::Directory,
 };
 
-fn print_directory(data: &[u8], address: u64, indent: usize) {
+fn print_directory(data: &[u8], address: u64, indent: usize, export_opt: Option<&PathBuf>) {
     //TODO: optimize
     let mut padding = String::with_capacity(indent);
     for i in 0..indent {
@@ -19,8 +19,15 @@ fn print_directory(data: &[u8], address: u64, indent: usize) {
             println!("{}* {:#X}: BIOS Directory", padding, address);
             for entry in directory.entries() {
                 println!("{}  * Type {:02X} Region {:02X} Flags {:02X} SubProg {:02X} Size {:08X} Source {:08X} Dest {:016X}: {}", padding, entry.kind, entry.region_kind, entry.flags, entry.sub_program, entry.size, entry.source, entry.destination, entry.description());
+                if let Some(export) = export_opt {
+                    let dir = export.join("bios").join("l1");
+                    fs::create_dir_all(&dir).expect("failed to create bios/l1 export directory");
+                    let file = dir.join(&format!("Type_{:02X}_Region_{:02X}_Flags_{:02X}_SubProg_{:02X}", entry.kind, entry.region_kind, entry.flags, entry.sub_program));
+                    let data = format!("Size {:08X}", entry.size);
+                    fs::write(&file, &data).expect("failed to write bios/l1 export file");
+                }
                 if entry.kind == 0x70 {
-                    print_directory(data, entry.source, indent + 4);
+                    print_directory(data, entry.source, indent + 4, export_opt);
                 }
             }
         },
@@ -28,21 +35,39 @@ fn print_directory(data: &[u8], address: u64, indent: usize) {
             println!("{}* {:#X}: BIOS Combo Directory", padding, address);
             for entry in combo.entries() {
                 println!("{}  * {:X?}", padding, entry);
-                print_directory(data, entry.directory, indent + 4);
+                print_directory(data, entry.directory, indent + 4, export_opt);
             }
         },
         Ok(Directory::BiosLevel2(directory)) => {
             println!("{}* {:#X}: BIOS Level 2 Directory", padding, address);
             for entry in directory.entries() {
                 println!("{}  * Type {:02X} Region {:02X} Flags {:02X} SubProg {:02X} Size {:08X} Source {:08X} Dest {:016X}: {}", padding, entry.kind, entry.region_kind, entry.flags, entry.sub_program, entry.size, entry.source, entry.destination, entry.description());
+                if let Some(export) = export_opt {
+                    let dir = export.join("bios").join("l2");
+                    fs::create_dir_all(&dir).expect("failed to create bios/l2 export directory");
+                    let file = dir.join(&format!("Type_{:02X}_Region_{:02X}_Flags_{:02X}_SubProg_{:02X}", entry.kind, entry.region_kind, entry.flags, entry.sub_program));
+                    let data = format!("Size {:08X}", entry.size);
+                    fs::write(&file, &data).expect("failed to write bios/l2 export file");
+                }
             }
         },
         Ok(Directory::Psp(directory)) => {
             println!("{}* {:#X}: PSP Directory", padding, address);
             for entry in directory.entries() {
                 println!("{}  * Type {:02X} SubProg {:02X} Size {:08X} Value {:08X}: {}", padding, entry.kind, entry.sub_program, entry.size, entry.value, entry.description());
+                if let Some(export) = export_opt {
+                    let dir = export.join("psp").join("l1");
+                    fs::create_dir_all(&dir).expect("failed to create psp/l1 export directory");
+                    let file = dir.join(format!("Type_{:02X}_SubProg_{:02X}", entry.kind, entry.sub_program));
+                    let data = if entry.size == 0xFFFFFFFF {
+                        format!("Value {:016X}", entry.value)
+                    } else {
+                        format!("Size {:08X}", entry.size)
+                    };
+                    fs::write(&file, &data).expect("failed to write psp/l1 export file");
+                }
                 if entry.kind == 0x40 {
-                    print_directory(data, entry.value, indent + 4);
+                    print_directory(data, entry.value, indent + 4, export_opt);
                 }
             }
         },
@@ -50,13 +75,24 @@ fn print_directory(data: &[u8], address: u64, indent: usize) {
             println!("{}* {:#X}: PSP Combo Directory", padding, address);
             for entry in combo.entries() {
                 println!("{}  * {:X?}", padding, entry);
-                print_directory(data, entry.directory, indent + 4);
+                print_directory(data, entry.directory, indent + 4, export_opt);
             }
         },
         Ok(Directory::PspLevel2(directory)) => {
             println!("{}* {:#X}: PSP Level 2 Directory", padding, address);
             for entry in directory.entries() {
                 println!("{}  * Type {:02X} SubProg {:02X} Size {:08X} Value {:08X}: {}", padding, entry.kind, entry.sub_program, entry.size, entry.value, entry.description());
+                if let Some(export) = export_opt {
+                    let dir = export.join("psp").join("l2");
+                    fs::create_dir_all(&dir).expect("failed to create psp/l2 export directory");
+                    let file = dir.join(&format!("Type_{:02X}_SubProg_{:02X}", entry.kind, entry.sub_program));
+                    let data = if entry.size == 0xFFFFFFFF {
+                        format!("Value {:016X}", entry.value)
+                    } else {
+                        format!("Size {:08X}", entry.size)
+                    };
+                    fs::write(&file, &data).expect("failed to write psp/l2 export file");
+                }
             }
         },
         Err(err) => {
@@ -69,8 +105,19 @@ fn main() {
     let file = if let Some(file) = env::args().nth(1) {
         file
     } else {
-        eprintln!("used_regions <file>");
+        eprintln!("used_regions <file> [export]");
         process::exit(1);
+    };
+
+    let export_opt = if let Some(export) = env::args().nth(2) {
+        let export = PathBuf::from(export);
+        if export.exists() {
+            fs::remove_dir_all(&export).expect("failed to clean export directory");
+        }
+        fs::create_dir(&export).expect("failed to create export directory");
+        Some(export)
+    } else {
+        None
     };
 
     let data = fs::read(file).unwrap();
@@ -80,6 +127,6 @@ fn main() {
     let signature = rom.signature();
     println!("{:#X?}", signature);
 
-    print_directory(&data, signature.psp as u64, 0);
-    print_directory(&data, signature.bios as u64, 0);
+    print_directory(&data, signature.psp as u64, 0, export_opt.as_ref());
+    print_directory(&data, signature.bios as u64, 0, export_opt.as_ref());
 }
